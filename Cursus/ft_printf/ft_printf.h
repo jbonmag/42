89daae2e4f24a75099590a6bd5859de0ef11d3bf
J. Bonet <jbonetm@proton.me>
2025-02-12 00:36:26 +0100

    Segunda versión. Sin bonus.
    Los archivos de la carpeta Main son tan solo para testear libftprintf.a, no serán entregados.

---
+++ Cursus/ft_printf/ft_printf.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubonet- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 00:16:10 by jubonet-          #+#    #+#             */
+/*   Updated: 2025/02/12 00:17:11 by jubonet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *format, ...);
+int		ft_format_case(va_list args, char c);
+
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_putnbr(int n);
+int		ft_putunsigned(unsigned int n);
+int		ft_putptr(void *ptr);
+int		ft_putnbr_base(unsigned long long int n, const char *base);
+size_t	ft_strlen(const char *str);
+
+#endif
---
+++ Cursus/ft_printf/ft_putnbr_base.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubonet- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/12 00:18:13 by jubonet-          #+#    #+#             */
+/*   Updated: 2025/02/12 00:18:18 by jubonet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (*(str++))
+		len++;
+	return (len);
+}
+
+int	ft_putnbr_base(unsigned long long int nbr, const char *base)
+{
+	int				count;
+	unsigned int	base_len;
+
+	count = 0;
+	base_len = ft_strlen(base);
+	if (nbr >= base_len)
+	{
+		count += ft_putnbr_base(nbr / base_len, base);
+		count += ft_putchar(base[nbr % base_len]);
+	}
+	else
+	{
+		count += ft_putchar(base[nbr]);
+	}
+	return (count);
+}
