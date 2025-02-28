48b9aa8e21c8fc4284cdaf46b5bf37dab7ee10d5
J. Bonet <jbonetm@proton.me>
2025-02-28 13:14:13 +0100

    ft_printf definitive edition 💫
    Enlaces en el makefile con libft corregidos. Modificada la libft --> Habían varios errores, entre ellos devolver void cuando debería haber sido int (ft_putnbr_fd, ft_putchar_fd), correcto manejo de NULL en ciertas funciones y corrección de ft_putnbr_fd para que retorne la cantidad de caracteres escritos. Se ha modificado libft.h en base a los cambios mencionados.

---
+++ Cursus/ft_printf/libft/ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubonet- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 17:20:06 by jubonet-          #+#    #+#             */
+/*   Updated: 2025/01/10 22:47:42 by jubonet-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
+}
