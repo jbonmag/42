477b85117c8d67301fc5f36b3fa732285f528c9e
J. Bonet <jbonetm@proton.me>
2025-03-26 11:17:31 +0100

    Update ft_strlen.c

--- Cursus/libft/ft_strlen.c
+++ Cursus/libft/ft_strlen.c
@@ -10,14 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-DESCRIPTION
-       The strlen() function calculates the length of the string pointed to by
-       s, excluding the terminating null byte ('\0').
-RETURN VALUE
-       The strlen() function returns the number of bytes in the string pointed
-       to by s.
-*/
 #include <stdlib.h>
 
 size_t	ft_strlen(const char *s)
@@ -29,9 +21,3 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
-/*int main(void)
-{
-    char    *s = "Amorenauer";
-    printf("%d\n", ft_strlen(s));
-    //El resultado debe ser = 10
-}*/
