360491d1dccdd758ac3fec8835a488259d647913
J. Bonet <jbonetm@proton.me>
2025-03-26 11:14:01 +0100

    Update ft_lstnew_bonus.c

--- Cursus/libft/ft_lstnew_bonus.c
+++ Cursus/libft/ft_lstnew_bonus.c
@@ -27,13 +27,3 @@ t_list	*ft_lstnew(void *content)
 }
 
 /*int	main(void)
-{
-	char	*str;
-
-	t_list	*elem;
-	str = (char *)malloc(6*sizeof(char));
-	str = "Hello";
-	elem = ft_lstnew((void *)str);
-	printf("%s\n", (char *)elem->content);
-	free(new->content);
-}*/
